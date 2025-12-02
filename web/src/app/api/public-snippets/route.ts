d2f40a7bce44c93abee7a26c0e2150c613acae8e
Piyush5784 <piyushjha5668@gmail.com>
2025-12-02 21:14:39 +0530

    fixed route

--- web/src/app/api/public-snippets/route.ts
+++ web/src/app/api/public-snippets/route.ts
@@ -21,8 +21,10 @@ export async function GET(req: NextRequest) {
     const snippets = await prisma.snippets.findMany({
       where: {
         isPublic: true,
+        user: {
+          is: {},
+        },
       },
-
       select: {
         id: true,
         title: true,
